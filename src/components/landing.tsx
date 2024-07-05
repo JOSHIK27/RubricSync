2e30c0b1382e7c9f4f81575d99232676fa107c44
JOSHIK <joshikroshan4021@gmail.com>
2024-07-05 09:58:49 +0100

    minor fix

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 
 export default function Landing({ userId }: { userId: any }) {
   return (
@@ -24,8 +25,9 @@ export default function Landing({ userId }: { userId: any }) {
         This is an AI tool which scans your research report and compares with
         the rubric to generate a comprehensive report with suggestions
       </h3>
+
       <div className="flex justify-center">
-        <Sheet>
+        {/* <Sheet>
           <motion.div
             initial={{ x: "-1vw" }}
             animate={{ x: "0vw" }}
@@ -41,30 +43,38 @@ export default function Landing({ userId }: { userId: any }) {
               <FileUpload />
             </Card>
           </SheetContent>
-        </Sheet>
+        </Sheet> */}
+        <Drawer>
+          <DrawerTrigger asChild>
+            <Button
+              variant="outline"
+              className="bg-[#4d4d50] shadow-lg  text-[#E6E4E3] border-0 hover:bg-[#3f3f4a] hover:text-white mt-8 px-12 py-2 text-[16px] rounded-none font-light"
+            >
+              Try Now
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <Card className={`mx-auto md:w-[500px] p-8 mb-12 mt-4`}>
+              <Context />
+              <FileUpload />
+            </Card>
+          </DrawerContent>
+        </Drawer>
         {!userId ? (
-          <motion.div
-            initial={{ x: "1vw" }}
-            animate={{ x: "0vw" }}
-            transition={{ ease: "linear", duration: 0.2 }}
-          >
+          <div>
             <Link href={"/sign-in"}>
               <Button className="border-1 border-neutral-600 ml-2 bg-[#F0EBE3] hover:bg-[#cbc6bf] shadow-lg  text-black mt-8 px-12 py-2 text-[16px] rounded-none">
                 Sign In
               </Button>
             </Link>
-          </motion.div>
+          </div>
         ) : (
           <SignOutButton>
-            <motion.div
-              initial={{ x: "1vw" }}
-              animate={{ x: "0vw" }}
-              transition={{ ease: "linear", duration: 0.2 }}
-            >
+            <div>
               <Button className="border-1 border-neutral-600 ml-2 bg-[#F0EBE3] hover:bg-[#cbc6bf] shadow-lg  text-black mt-8 px-12 py-2 text-[16px] rounded-none">
                 Sign Out
               </Button>
-            </motion.div>
+            </div>
           </SignOutButton>
         )}
       </div>

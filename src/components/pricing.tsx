79a5aef2d62e8f54b360b2b7bc7040fd5b6f094b
JOSHIK <joshikroshan4021@gmail.com>
2024-07-25 22:40:35 +0100

    framer motion animations

--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -1,5 +1,6 @@
 import { Card } from "./ui/card";
 import { Button } from "./ui/button";
+import { motion } from "framer-motion";
 export default function Pricing() {
   return (
     <section className="mb-20">
@@ -12,160 +13,172 @@ export default function Pricing() {
         </h2>
       </div>
       <div className="flex justify-center flex-wrap">
-        <Card className="p-8 m-6 w-[400px] border-[1px] transition-all duration-300 ease-in-out hover:scale-105 border-[#d3d1ce] bg-[#e8e6e5]">
-          <h1 className="text-[22px] font-[950] text-[#484642]">Basic</h1>
-          <h2 className="text-[40px] font-[950] text-[#484642]">Free</h2>
-          <h2 className="text-[16px] text-[#484642] font-[600]">
-            Includes all key features to kick start
-          </h2>
-          <br />
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1.5em"
-              height="1.5em"
-              viewBox="0 0 24 24"
-              className="mr-1"
-            >
-              <path
-                fill="#3f3e3a"
-                d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
-              />
-            </svg>
-            <span className="text-[14px] text-[#484642] font-[500]">
-              Upload upto 50 pages of report
-            </span>
-          </div>
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1.5em"
-              height="1.5em"
-              viewBox="0 0 24 24"
-              className="mr-1"
-            >
-              <path
-                fill="#3f3e3a"
-                d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
-              />
-            </svg>
-            <span className="text-[14px] text-[#484642] font-[500]">
-              Upload upto 50 pages of report
-            </span>
-          </div>
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1.5em"
-              height="1.5em"
-              viewBox="0 0 24 24"
-              className="mr-1"
-            >
-              <path
-                fill="#3f3e3a"
-                d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
-              />
-            </svg>
-            <span className="text-[14px] text-[#484642] font-[500]">
-              Upload upto 50 pages of report
-            </span>
-          </div>
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1.5em"
-              height="1.5em"
-              viewBox="0 0 24 24"
-              className="mr-1"
-            >
-              <path
-                fill="#3f3e3a"
-                d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
-              />
-            </svg>
-            <span className="text-[14px] text-[#484642] font-[500]">
-              Upload upto 50 pages of report
-            </span>
-          </div>
-          <Button className="mt-8 w-full bg-[#282624]">Get Started</Button>
-        </Card>
-        <Card className="p-8 m-6 w-[400px] border-[1px] border-[#d3d1ce] bg-[#e8e6e5] transition-all duration-300 ease-in-out hover:scale-105">
-          <h1 className="text-[22px] font-[950] text-[#484642]">Premium</h1>
-          <h2 className="text-[40px] font-[950] text-[#484642]">Soon</h2>
-          <h2 className="text-[16px] text-[#484642] font-[600]">
-            Includes all key features to kick start
-          </h2>
-          <br />
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1.5em"
-              height="1.5em"
-              viewBox="0 0 24 24"
-              className="mr-1"
-            >
-              <path
-                fill="#3f3e3a"
-                d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
-              />
-            </svg>
-            <span className="text-[14px] text-[#484642] font-[500]">
-              Upload upto 50 pages of report
-            </span>
-          </div>
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1.5em"
-              height="1.5em"
-              viewBox="0 0 24 24"
-              className="mr-1"
-            >
-              <path
-                fill="#3f3e3a"
-                d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
-              />
-            </svg>
-            <span className="text-[14px] text-[#484642] font-[500]">
-              Upload upto 50 pages of report
-            </span>
-          </div>
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1.5em"
-              height="1.5em"
-              viewBox="0 0 24 24"
-              className="mr-1"
-            >
-              <path
-                fill="#3f3e3a"
-                d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
-              />
-            </svg>
-            <span className="text-[14px] text-[#484642] font-[500]">
-              Upload upto 50 pages of report
-            </span>
-          </div>
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="1.5em"
-              height="1.5em"
-              viewBox="0 0 24 24"
-              className="mr-1"
-            >
-              <path
-                fill="#3f3e3a"
-                d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
-              />
-            </svg>
-            <span className="text-[14px] text-[#484642] font-[500]">
-              Upload upto 50 pages of report
-            </span>
-          </div>
-          <Button className="mt-8 w-full bg-[#282624]">Get Started</Button>
-        </Card>
+        <motion.div
+          initial={{ y: 100 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+        >
+          <Card className="p-8 m-6 w-[400px] border-[1px] transition-all duration-300 ease-in-out hover:scale-105 border-[#d3d1ce] bg-[#e8e6e5]">
+            <h1 className="text-[22px] font-[950] text-[#484642]">Basic</h1>
+            <h2 className="text-[40px] font-[950] text-[#484642]">Free</h2>
+            <h2 className="text-[16px] text-[#484642] font-[600]">
+              Includes all key features to kick start
+            </h2>
+            <br />
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+                className="mr-1"
+              >
+                <path
+                  fill="#3f3e3a"
+                  d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
+                />
+              </svg>
+              <span className="text-[14px] text-[#484642] font-[500]">
+                Upload upto 50 pages of report
+              </span>
+            </div>
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+                className="mr-1"
+              >
+                <path
+                  fill="#3f3e3a"
+                  d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
+                />
+              </svg>
+              <span className="text-[14px] text-[#484642] font-[500]">
+                Upload upto 50 pages of report
+              </span>
+            </div>
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+                className="mr-1"
+              >
+                <path
+                  fill="#3f3e3a"
+                  d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
+                />
+              </svg>
+              <span className="text-[14px] text-[#484642] font-[500]">
+                Upload upto 50 pages of report
+              </span>
+            </div>
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+                className="mr-1"
+              >
+                <path
+                  fill="#3f3e3a"
+                  d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
+                />
+              </svg>
+              <span className="text-[14px] text-[#484642] font-[500]">
+                Upload upto 50 pages of report
+              </span>
+            </div>
+            <Button className="mt-8 w-full bg-[#282624]">Get Started</Button>
+          </Card>
+        </motion.div>
+        <motion.div
+          initial={{ y: 100 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+        >
+          <Card className="p-8 m-6 w-[400px] border-[1px] border-[#d3d1ce] bg-[#e8e6e5] transition-all duration-300 ease-in-out hover:scale-105">
+            <h1 className="text-[22px] font-[950] text-[#484642]">Premium</h1>
+            <h2 className="text-[40px] font-[950] text-[#484642]">Soon</h2>
+            <h2 className="text-[16px] text-[#484642] font-[600]">
+              Includes all key features to kick start
+            </h2>
+            <br />
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+                className="mr-1"
+              >
+                <path
+                  fill="#3f3e3a"
+                  d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
+                />
+              </svg>
+              <span className="text-[14px] text-[#484642] font-[500]">
+                Upload upto 50 pages of report
+              </span>
+            </div>
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+                className="mr-1"
+              >
+                <path
+                  fill="#3f3e3a"
+                  d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
+                />
+              </svg>
+              <span className="text-[14px] text-[#484642] font-[500]">
+                Upload upto 50 pages of report
+              </span>
+            </div>
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+                className="mr-1"
+              >
+                <path
+                  fill="#3f3e3a"
+                  d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
+                />
+              </svg>
+              <span className="text-[14px] text-[#484642] font-[500]">
+                Upload upto 50 pages of report
+              </span>
+            </div>
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+                className="mr-1"
+              >
+                <path
+                  fill="#3f3e3a"
+                  d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"
+                />
+              </svg>
+              <span className="text-[14px] text-[#484642] font-[500]">
+                Upload upto 50 pages of report
+              </span>
+            </div>
+            <Button className="mt-8 w-full bg-[#282624]">Get Started</Button>
+          </Card>
+        </motion.div>
       </div>
     </section>
   );

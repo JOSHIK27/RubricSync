2c125906a21db814187f422eefc2d16b3cd2d1e8
JOSHIK <joshikroshan4021@gmail.com>
2024-09-12 20:30:48 +0100

    stripe integrated

---
+++ src/app/api/webhooks/stripe/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+import { supabase } from "@/lib";
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "");
+
+  const signature = req.headers.get("Stripe-Signature");
+
+  try {
+    const event = stripe.webhooks.constructEvent(
+      body,
+      signature ?? "",
+      endpointSecret ?? ""
+    );
+
+    if (event.type === "payment_intent.succeeded") {
+    }
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { message: "Webhook signature verification failed" },
+      { status: 400 }
+    );
+  }
+
+  return NextResponse.json({ message: "Webhook received" });
+}

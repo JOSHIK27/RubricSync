f61e895cb35a89c6a320410b9c41ddb1e6a7c2b9
JOSHIK <joshikroshan4021@gmail.com>
2024-10-02 13:01:05 +0100

    reduxtool kit initialised

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,18 +2,30 @@
 import { feedbackContext, workspaceContext } from "./context/AppContext";
 import { ClerkProvider } from "@clerk/nextjs";
 import { useState } from "react";
+import { useRef } from "react";
+import { Provider } from "react-redux";
+import { makeStore, AppStore } from "../lib/store";
+
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [feedback, setFeedback] = useState<any>(null);
   const [workspaceCount, setWorkspaceCount] = useState(0);
+  const storeRef = useRef<AppStore>();
+  if (!storeRef.current) {
+    // Create the store instance the first time this renders
+    storeRef.current = makeStore();
+  }
+
   return (
-    <ClerkProvider>
-      <feedbackContext.Provider value={{ feedback, setFeedback }}>
-        <workspaceContext.Provider
-          value={{ workspaceCount, setWorkspaceCount }}
-        >
-          {children}
-        </workspaceContext.Provider>
-      </feedbackContext.Provider>
-    </ClerkProvider>
+    <Provider store={storeRef.current}>
+      <ClerkProvider>
+        <feedbackContext.Provider value={{ feedback, setFeedback }}>
+          <workspaceContext.Provider
+            value={{ workspaceCount, setWorkspaceCount }}
+          >
+            {children}
+          </workspaceContext.Provider>
+        </feedbackContext.Provider>
+      </ClerkProvider>
+    </Provider>
   );
 }

7d6f48689849af1b988db099fb65b5a262e02a62
JOSHIK <joshikroshan4021@gmail.com>
2025-02-02 16:52:08 +0000

    set runtime to edge

--- src/app/api/feedback/route.ts
+++ src/app/api/feedback/route.ts
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 import { NextResponse } from "next/server";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { Document } from "@langchain/core/documents";
